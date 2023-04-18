ca4a651d3c188d588c8de7e90a05688b7111e469
hajjou walkadi <walkadihajjou153@gmail.com>
2023-04-18 09:40:05 +0000

    modify contact model

--- app/Models/contact.php
+++ app/Models/contact.php
@@ -11,7 +11,7 @@ class Contact extends Model
 {
     use HasFactory;
   
-    public $fillable = ['name', 'email','message'];
+    public $fillable = ['name', 'email', 'message'];
   
     /**
      * Write code on Method
