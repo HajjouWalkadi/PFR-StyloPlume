f5d214afee3ece3cae49b2c4152c536ef94ba0b2
hajjou walkadi <walkadihajjou153@gmail.com>
2023-04-20 13:23:02 +0000

    make changes in product table

--- database/migrations/2023_03_22_234556_create_products_table.php
+++ database/migrations/2023_03_22_234556_create_products_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
             $table->unsignedFloat('price')->nullable();
             $table->float('discount_price')->nullable();
             $table->unsignedBigInteger('category_id')->index();
-            $table->foreign('category_id')->references('id')->on('categories');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
             $table->timestamps();
         });
     }
